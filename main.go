b5267af1e6a0856bfe8edcfbc5ffb90ca20164b8
Mateen <mateenqazi125@gmail.com>
2023-08-04 14:56:16 +0500

    implementation of swagger

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/mateenqazi/jwt-authenication/controllers"
+	_ "github.com/mateenqazi/jwt-authenication/docs"
 	"github.com/mateenqazi/jwt-authenication/initializers"
 	"github.com/mateenqazi/jwt-authenication/middleware"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() {
@@ -13,8 +16,16 @@ func init() {
 	initializers.SyncDatabase()
 }
 
+// @title Tag Service API
+// @version 1.0
+// @description A Tag service API in Go using Gin framework
+
+// @host localhost:3000
+// @BasePath /
 func main() {
 	r := gin.Default()
+
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.POST("/signup", controllers.Signup)
 	r.POST("/login", controllers.Login)
